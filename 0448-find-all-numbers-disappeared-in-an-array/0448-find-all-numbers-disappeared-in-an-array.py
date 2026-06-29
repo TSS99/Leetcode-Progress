98b0b474348cb44aab25308cfdc2e1d1fdf7374c
Tilock Sadhukhan <147226050+TSS99@users.noreply.github.com>
2026-06-29 10:55:52 +0530

    Time: 22 ms (80.14%), Space: 30.2 MB (98.1%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,11 +1,17 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         seen = set(nums)
+        # What this line is doing is basically, it is removing all the repeated/duplicate items in the "nums" array
 
         answer = []
+        # What this line is doing is, it is creating an empty array called answer, that will be an empty array.
 
         for i in range(1, len(nums) + 1):
-            if i not in seen:
-                answer.append(i)
+        #This line is what is doing is bacically creating a loop, which will be running from 1 to the length of the array+1 , since the loop is starting from 1 and not from zero. also the limit is set such that, i will be running from 1 to lenth of the array+1 because it includes the first element but excludes the last element3,4,5,6
 
+            if i not in seen:
+            # Then it is manually checking whether i is not in the list seen then, 
+                answer.append(i)
+                # Add the i in the answer list, that is what it is doing, and keep on adding
         return answer
+        # And finally the function is returning the answer.
